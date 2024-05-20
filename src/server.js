dc56571666c31d5b59cbe80e4ed6bbc192b2e368
IvoCorrea <ivoaurelio30@gmail.com>
2024-05-20 19:27:17 -0300

    public path changed

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const app = express()
 
 const appHome = require('./routes/home')
 const askTo = require('./routes/askTo')
-const publicPath = path.join(__dirname, 'public')
 
-app.use(express.static(publicPath))
+app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
